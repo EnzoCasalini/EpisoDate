04189fdc05206fabfb52ccb0a41807d6c560dba5
Enzo Casalini <enzocasa83@gmail.com>
2022-04-07 23:54:37 +0200

    factorisation

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,49 +1,15 @@
 import {
-    IonCard,
     IonContent,
-    IonHeader, IonImg,
-    IonInfiniteScroll,
-    IonInfiniteScrollContent, IonItem, IonLabel, IonList,
-    IonPage, IonText,
+    IonHeader,
+    IonPage,
     IonTitle,
-    IonToolbar, useIonViewWillEnter
+    IonToolbar,
 } from '@ionic/react';
 import './Tab1.css';
 import {useState} from "react";
-import {EpisoDateAPI} from "../api/results/EpisoDateAPI";
+import SerieComponent from "../components/SerieComponent";
 
 const Tab1: React.FC = () => {
-
-    const [isInfiniteDisabled, setIsInfiniteDisabled] = useState<boolean>(false);
-    const [allSeries, setAllSeries] = useState<EpisoDateAPI>({
-        page: 0,
-        pages: 0,
-        total: '0',
-        tv_shows: []
-    });
-
-    async function GetAllSeries (url:string) {
-        try {
-            setIsInfiniteDisabled(true);
-
-            const response = await fetch(url);
-            const data = await response.json() as EpisoDateAPI;
-
-            setAllSeries({
-                page: data.page,
-                pages: data.pages,
-                total: data.total,
-                tv_shows: [...allSeries.tv_shows, ...data.tv_shows]
-            });
-
-            setIsInfiniteDisabled(false);
-        }
-        catch (e) {
-            console.error(e);
-        }
-    }
-
-    useIonViewWillEnter(async () => await GetAllSeries(`https://www.episodate.com/api/search?page=1`))
 
     return (
     <IonPage>
@@ -53,34 +19,7 @@ const Tab1: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-          <IonList>
-              {
-                  allSeries?.tv_shows.map((value) => {
-                      return(
-                          <>
-                          <IonCard>
-                              <IonImg src={value.image_thumbnail_path}/>
-                              <IonItem>
-                                  <IonLabel>{value.name}</IonLabel>
-                              </IonItem>
-                          </IonCard>
-
-                          </>
-                      )
-                  })
-              }
-          </IonList>
-          {/* On active le scroll infini. */ }
-          <IonInfiniteScroll
-              onIonInfinite={async () => await GetAllSeries(`https://www.episodate.com/api/search?page=${allSeries?.page + 1}` as string)}
-              threshold="100px"
-              disabled={isInfiniteDisabled}
-          >
-              <IonInfiniteScrollContent
-                  loadingSpinner="bubbles"
-                  loadingText="Loading more data..."
-              />
-          </IonInfiniteScroll>
+          <SerieComponent/>
       </IonContent>
     </IonPage>
     );
